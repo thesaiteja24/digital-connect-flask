ceca555094c585242da85e596b997d9f6e51f9db
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-01-07 20:25:41 +0530

    Refactor login and registration routes, add home route, and update templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask_bcrypt import Bcrypt
+from bson.objectid import ObjectId
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
 import certifi
@@ -14,8 +15,21 @@ mongo = PyMongo(app).db
 bcrypt = Bcrypt(app)
 
 
-@app.route("/")
-def render_login():
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = mongo.users.find_one({"email": email})
+
+        if user and bcrypt.check_password_hash(user['password'], password):
+            session['user_id'] = str(user['_id'])
+            session['email'] = email
+            session['username'] = user['username']  # Store username in session
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid email or password. Please try again.", "danger")
+
     return render_template('login.html')
 
 
@@ -35,32 +49,45 @@ def login():
 
     return render_template('login.html')
 
+
 @ app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == 'POST':
-        username=request.form['username']
-        password=request.form['password']
-        email=request.form["email"]
-        role=request.form["role"]
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form["email"]
+        role = request.form["role"]
 
-        existing_user=mongo.users.find_one({"email": email})
+        existing_user = mongo.users.find_one({"email": email})
         if existing_user:
             return "Username already exists. Please choose another."
 
-        hashed_password=bcrypt.generate_password_hash(
+        hashed_password = bcrypt.generate_password_hash(
             password).decode('utf-8')
         mongo.users.insert_one(
             {"username": username, "email": email, "password": hashed_password, "role": role})
 
-        return redirect(url_for('user_routes.render_login'))
+        return redirect(url_for('render_login'))
 
     return render_template('register.html')
 
+
 @ app.route("/logout")
 def logout():
-    session.pop('user_id', None)
-    session.pop('username', None)
+    session.clear()
     return redirect(url_for('login'))
+
+
+@app.route("/home")
+def home():
+    if 'user_id' in session:  # Check if the user is logged in
+        user = mongo.users.find_one(
+            {"_id": ObjectId(session['user_id'])})  # Get user from DB
+        if user:
+            username = user['username']
+            return render_template('home.html', username=username)
+    return redirect(url_for('login'))  # Redirect to login if not logged in
+
 
 if __name__ == '__main__':
     app.run()
